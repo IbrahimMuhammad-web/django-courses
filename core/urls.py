6741d2bd809823c7a4306c7877b4e1a07b9130b3
Vijay Thapa <vijaythapa333@gmail.com>
2020-09-15 16:51:23 +0545

    Profile Update Complete

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('accounts/profile/', views.profile, name="profile"),
+    path('accounts/update-profile/', views.update_profile, name="update-profile"),
+    path('accounts/save-profile/', views.save_profile, name="save-profile"),
 ]
