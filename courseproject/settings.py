e004ebc194e84755fe0c621aa621d95c094cd0c7
Vijay Thapa <vijaythapa333@gmail.com>
2020-09-17 21:22:09 +0545

    using python-dotenv

--- courseproject/settings.py
+++ courseproject/settings.py
@@ -1,6 +1,13 @@
 import os
 from django.contrib import messages # To change ERROR into Danger
 
+# import environ # To use .env file
+
+# using python-dotenv
+from dotenv import load_dotenv
+load_dotenv()
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,6 +148,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Using .env
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 
 
 
